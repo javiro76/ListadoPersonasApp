262c4777aad35bc3a59960a1d8962cfbdd51407f
javier <javier.rodriguez@dev-codes.io>
2025-02-05 21:17:57 -0500

    ADD: setters and getters

--- src/Persona.java
+++ src/Persona.java
@@ -20,4 +20,52 @@ public class Persona {
         this.email = email;
     }
 
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                '}'+ " " + "direccionMemoria:" + super.toString();
+    }
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Javier","2344","javir@gmil.com");
+
+        System.out.println(persona1);
+
+    }
 }
